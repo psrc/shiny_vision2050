2d8d7444f87489c0b9fb417af562b84565232711
Hana Sevcikova <hsevcikova@psrc.org>
2018-07-17 15:51:38 -0700

    path changed

--- shiny/global.R
+++ shiny/global.R
@@ -15,16 +15,11 @@ library(tidyverse)
 # )
 
 # local
-#base <- list(Modelsrv5 = "//modelsrv5/d$/opusgit/urbansim_data/data/psrc_parcel",
-#             Modelsrv6 = "//modelsrv6/d$/opusgit/urbansim_data/data/psrc_parcel",
-#             Modelsrv8 = "//MODELSRV8/d$/opusgit/urbansim_data/data/psrc_parcel",
-#             Modelsrv3 = "//modelsrv3/e$/opusgit/urbansim_data/data/psrc_parcel"
-#             )
-base <- list(Modelsrv5 = "/Users/hana/d5$/opusgit/urbansim_data/data/psrc_parcel",
-             Modelsrv6 = "/Users/hana/d6$/opusgit/urbansim_data/data/psrc_parcel"
-             #Modelsrv8 = "//MODELSRV8/d$/opusgit/urbansim_data/data/psrc_parcel",
-             #Modelsrv3 = "//modelsrv3/e$/opusgit/urbansim_data/data/psrc_parcel"
-)
+base <- list(Modelsrv5 = "//modelsrv5/d$/opusgit/urbansim_data/data/psrc_parcel",
+             Modelsrv6 = "//modelsrv6/d$/opusgit/urbansim_data/data/psrc_parcel",
+             Modelsrv8 = "//MODELSRV8/d$/opusgit/urbansim_data/data/psrc_parcel",
+             Modelsrv3 = "//modelsrv3/e$/opusgit/urbansim_data/data/psrc_parcel"
+             )
 
 # indicator/output directories
 lu.run <- file.path(base$Modelsrv6, "runs", "run_134.run_2018_05_12_13_11", "indicators")
