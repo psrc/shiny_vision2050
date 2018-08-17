34cb01dbbadf393a181f7322ad86a3286d0ece74
Christy Lam <cyyllam@gmail.com>
2018-08-17 13:38:09 -0700

    reconfigured to read more than one run

--- scripts/housing_type_mix.R
+++ scripts/housing_type_mix.R
@@ -1,16 +1,26 @@
 library(data.table)
 
-# hit 'source'
-this.dir <- dirname(parent.frame(2)$ofile)
-setwd(this.dir)
-source("all_runs.R")
+# Aws-model03 - DUG - \\aws-model03\e$\opusgit\urbansim_data\data\psrc_parcel\runs\run_22.run_2018_08_10_21_05
+# Aws-model04 - H2O2 - \\aws-model04\e$\opusgit\urbansim_data\data\psrc_parcel\runs\run_1.run_2018_08_10_21_05
+# Aws-model05 - TOD - \\aws-model05\e$\opusgit\urbansim_data\data\psrc_parcel\runs\run_3.run_2018_08_10_21_04
+
 
 # user input --------------------------------------------------------------
 
-run.dir <- c("run_2.run_2018_07_17_15_22")
+this.dir <- "C:/Users/CLam/Desktop/shiny_vision2050/scripts" 
+# this.dir <-  dirname(parent.frame(2)$ofile)
+setwd(this.dir)
+source("all_runs.R")
+
+# urbansim
+run.dir <- c("DUG" = "run_22.run_2018_08_10_21_05", "H2O" = "run_1.run_2018_08_10_21_05", "TOD" = "run_3.run_2018_08_10_21_04")
+years <- c(2050) # only one year
+
 out.dir <- "../scripts_results"
+dsa.dir <- "X:/DSA/Vision2050/land_use_tables"
 data.dir <- file.path("../data")
-years <- c(2050)
+
+out.file.nm <- "housing_type_mix"
 
 # lookup table
 constraints <- fread(file.path(data.dir, "development_constraints.csv"))
@@ -20,34 +30,42 @@ constraints <- fread(file.path(data.dir, "development_constraints.csv"))
 
 # the density brackets
 density.split <- list(res = c(10, 45), nonres = c(1, 3))
+filename <- "parcel__dataset_table__households_jobs__2050.tab" 
 
-# load parcels - this will be an indicator file
-base.dir <- purrr::pluck(allruns, run.dir)
-filename <- "parcel__dataset_table__households_jobs__2050.tab"
-pcl <- fread(file.path(base.dir, "indicators", filename))
-
-# get the maximum density
-constr <- constraints[, .(max_dens = max(maximum)), by = .(plan_type_id, constraint_type)]
-
-# derive max residential and non-res density
-pcl[constr[constraint_type == "units_per_acre"], max_res_density := i.max_dens, on = "plan_type_id"]
-# pcl[constr[constraint_type == "far"], max_nonres_density := i.max_dens, on = "plan_type_id"]
-
-# categorize into three groups
-pcl[, res_density_type := factor(ifelse(max_res_density < density.split$res[1], "low",
-                                        ifelse(max_res_density >= density.split$res[2], "high", "med")),
-                                 levels = c("low", "med", "high"))]
-# pcl[, nonres_density_type := factor(ifelse(max_nonres_density < density.split$nonres[1], "low",
-#                                            ifelse(max_nonres_density >= density.split$nonres[2], "high", "med")),
-#                                     levels = c("low", "med", "high"))]
-
-cols <- c("residential_units", "residential_units_base")
-df <- pcl[!is.na(res_density_type), lapply(.SD, sum), .SDcols = cols, by = "res_density_type"][order(res_density_type)] 
-df[, delta := residential_units - residential_units_base][, sum_delta := sum(delta)][, share_delta := delta/sum_delta]
+dlist <- NULL
+for (r in 1:length(run.dir)) { # for each run
+  pcl <- df <- NULL
+  base.dir <- purrr::pluck(allruns, run.dir[r]) 
+  
+  # load parcels - this will be an indicator file
+  pcl <- fread(file.path(base.dir, "indicators", filename))
+  
+  # get the maximum density
+  constr <- constraints[, .(max_dens = max(maximum)), by = .(plan_type_id, constraint_type)]
+  
+  # derive max residential and non-res density
+  pcl[constr[constraint_type == "units_per_acre"], max_res_density := i.max_dens, on = "plan_type_id"]
+  # pcl[constr[constraint_type == "far"], max_nonres_density := i.max_dens, on = "plan_type_id"]
+  
+  # categorize into three groups
+  pcl[, res_density_type := factor(ifelse(max_res_density < density.split$res[1], "low",
+                                          ifelse(max_res_density >= density.split$res[2], "high", "med")),
+                                   levels = c("low", "med", "high"))]
+  # pcl[, nonres_density_type := factor(ifelse(max_nonres_density < density.split$nonres[1], "low",
+  #                                            ifelse(max_nonres_density >= density.split$nonres[2], "high", "med")),
+  #                                     levels = c("low", "med", "high"))]
+  
+  cols <- c("residential_units", "residential_units_base")
+  df <- pcl[!is.na(res_density_type), lapply(.SD, sum), .SDcols = cols, by = "res_density_type"][order(res_density_type)]
+  df[, delta := residential_units - residential_units_base
+     ][, sum_delta := sum(delta)
+       ][, share_delta := delta/sum_delta
+         ][, `:=` (run = run.dir[r], scenario = names(run.dir[r]))]
+  setcolorder(df, c("run", "scenario", colnames(df)[1:6]))
+  dlist[[names(run.dir[r])]] <- df
+}
 
 
 # export ------------------------------------------------------------------
 
-# write.xlsx(d2, file.path(out.dir, "housing_type_mix.xlsx"))
-write.csv(df, file.path(out.dir, "housing_type_mix.csv"), row.names = FALSE)
-write.csv(df, file.path("X:/DSA/Vision2050/land_use_tables", "housing_type_mix.csv"), row.names = FALSE)
+write.xlsx(dlist, file.path(dsa.dir, paste0(out.file.nm, "_", Sys.Date(), ".xlsx")))
