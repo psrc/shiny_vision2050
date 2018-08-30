0b975559c5f62e0a2ac83b6734590403d0d268be
Christy Lam <cyyllam@gmail.com>
2018-08-30 15:29:44 -0700

    switch base to settings

--- screening_factors/settings.R
+++ screening_factors/settings.R
@@ -3,11 +3,25 @@
 # Aws-model05 - iH2O2 - \\aws-model04\e$\opusgit\urbansim_data\data\psrc_parcel\runs\run_4.run_2018_08_17_16_15
 # Aws-model05 - TOD - \\aws-model05\e$\opusgit\urbansim_data\data\psrc_parcel\runs\run_3.run_2018_08_10_21_04
 
+base <- list(Aws01 = "//aws-model01/e$/opusgit/urbansim_data/data/psrc_parcel/runs",
+             Aws02 = "//aws-model02/e$/opusgit/urbansim_data/data/psrc_parcel/runs",
+             Aws03 = "//aws-model03/e$/opusgit/urbansim_data/data/psrc_parcel/runs",
+             Aws04 = "//aws-model04/e$/opusgit/urbansim_data/data/psrc_parcel/runs",
+             Aws05 = "//aws-model05/e$/opusgit/urbansim_data/data/psrc_parcel/runs",
+             Aws06 = "//aws-model06/e$/opusgit/urbansim_data/data/psrc_parcel/runs",
+             Aws07 = "//aws-model07/e$/opusgit/urbansim_data/data/psrc_parcel/runs",
+             Aws08 = "//aws-model08/e$/opusgit/urbansim_data/data/psrc_parcel/runs",
+             Aws09 = "//aws-model09/e$/opusgit/urbansim_data/data/psrc_parcel/runs",
+             Modelsrv5 = "//modelsrv5/d$/opusgit/urbansim_data/data/psrc_parcel/runs",
+             Modelsrv6 = "//modelsrv6/d$/opusgit/urbansim_data/data/psrc_parcel/runs",
+             Modelsrv8 = "//MODELSRV8/d$/opusgit/urbansim_data/data/psrc_parcel/runs",
+             Modelsrv3 = "//modelsrv3/e$/opusgit/urbansim_data/data/psrc_parcel/runs"
+)
+
 run.dir <- c("iSTC" = "run_3.run_2018_08_17_13_06",
              "iDUG" = "run_1.run_2018_08_17_15_45",
              "iH2O2" = "run_4.run_2018_08_17_16_15",
              "TOD" = "run_3.run_2018_08_10_21_04")
-# )
 
 out.dir <- "X:/DSA/Vision2050/land_use_tables"
           
@@ -28,6 +42,7 @@ settings <- list(goa = list(out.file.nm = "79_dist_growth_opp_areas"),
                  pjta = list(out.file.nm = "30_jobs_pop_tod_areas"),
                  jhr = list(out.file.nm = "18_jobs_housing_ratio")
                  ) 
+
 
 
 
