311816be469b14cfd080c50d373edd16dbc024fd
Christy Lam <cyyllam@gmail.com>
2018-08-31 14:16:21 -0700

    added settings and revised outdir for preliminary files

--- full_set/settings.R
+++ full_set/settings.R
@@ -1,6 +1,7 @@
 library(openxlsx)
 
 rund <- "/Volumes/Model\ Data/vision2050/opusgit/urbansim_data/data/psrc_parcel/runs"
+# rund <- "L:/vision2050/opusgit/urbansim_data/data/psrc_parcel/runs"
 base <- list(Aws01 = file.path(rund, "awsmodel01"),
              Aws03 = file.path(rund, "awsmodel03"),
              Aws04 = file.path(rund, "awsmodel04"),
@@ -8,8 +9,7 @@ base <- list(Aws01 = file.path(rund, "awsmodel01"),
              Aws06 = file.path(rund, "awsmodel06"),
              Aws07 = file.path(rund, "awsmodel07")
 )
-script.dir <- "/Users/hana/R/vision2050indicators/full_set" 
-
+script.dir <- "/Users/hana/R/vision2050indicators/full_set"
 run.dir <- c("iSTC" = "run_3.run_2018_08_17_13_06",
              "iDUG" = "run_1.run_2018_08_17_15_45", 
              "iH2O2" = "run_4.run_2018_08_17_16_15"#, 
@@ -18,7 +18,9 @@ run.dir <- c("iSTC" = "run_3.run_2018_08_17_13_06",
 indicator.dirnm <- "indicators"
 #data.dir <- "../data"
 data.dir <- "/Volumes/DataTeam/Projects/V2050/SEIS/Data_Support/script_input"
-out.dir <- "/Volumes/DataTeam/Projects/V2050/SEIS/Data_Support/Model_Output"
+out.dir <- "/Volumes/DataTeam/Projects/V2050/SEIS/Data_Support/Model_Output/Working"
+# data.dir <- "J:/Projects/V2050/SEIS/Data_Support/script_input"
+# out.dir <- "J:/Projects/V2050/SEIS/Data_Support/Model_Output/Working"
 
 years <- c(2050)
 byr <- 2017
